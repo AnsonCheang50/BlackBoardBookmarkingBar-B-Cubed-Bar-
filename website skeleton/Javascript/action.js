d0a565e02030e93f050d212d69465928a1464b9a
scalla1 <99899253+scalla1@users.noreply.github.com>
2022-11-13 12:16:15 -0500

    added bookmark bar to all pages, put code in one place. Basic link functionality added aswell

--- website skeleton/Javascript/action.js
+++ website skeleton/Javascript/action.js
@@ -1,3 +1,51 @@
+//Storage of links, static values for now
+
+let bookmarks = [
+  {
+    "link" : "../HTML/classexample.html",
+    "name" : "My Class"
+  },
+  {
+    "link" : "../HTML/assignmentexample.html",
+    "name" : "Bookmarked Assignment"
+  }
+  ]
+
+window.onload = function () {addLinks()};
+window.onscroll = function() {barScroll()};
+
+
+// Get the navbar
+var navbar = document.getElementById("bookmarkbar");
+var sticky = navbar.offsetTop;
+
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function barScroll() {
+  // Get the offset position of the navbar
+  
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function addLinks() {
+
+
+  var htmlString = "";
+  for (let i = 0; i < bookmarks.length; i++) {
+    htmlString = htmlString + "<a href=\"" + bookmarks[i].link + "\">" + bookmarks[i].name + "</a>\n";
+  }
+  console.log(htmlString);
+
+  document.getElementById('bookmarkbar').innerHTML = htmlString;
+}
+
+
+
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
@@ -15,3 +63,4 @@ function myFunction() {
       }
     }
   }
+
