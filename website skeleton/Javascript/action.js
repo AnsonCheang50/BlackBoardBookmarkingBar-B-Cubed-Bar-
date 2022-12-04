622f691123b6ef1879546617fe8aa9f02dbf60e9
AnsonCheang50 <113309258+AnsonCheang50@users.noreply.github.com>
2022-12-03 22:22:12 -0500

    Update action.js

--- website skeleton/Javascript/action.js
+++ website skeleton/Javascript/action.js
@@ -4,29 +4,29 @@
 
 // sample data
 let sampleListObj = [{
-  type : "Bookmark",
-  obj : {
-     link : "../HTML/classexample.html",
-     name : "My Class"
-  }
+   type : "Bookmark",
+   obj : {
+      link : "../HTML/classexample.html",
+      name : "My Class"
+   }
 }, {
-  type : "Bookmark",
-  obj : {
-     link : "../HTML/assignmentexample.html",
-     name : "My Assignment"
-  }
+   type : "Bookmark",
+   obj : {
+      link : "../HTML/assignmentexample.html",
+      name : "My Assignment"
+   }
 
 }, {
-  name : "Class Folder",
-  type : "Folder",
-  obj : 
-     [{
-        link : "../HTML/classexample.html",
-        name : "My Class"
-     }, {
-        link : "../HTML/classexample.html",
-        name : "My Class"
-     }]
+   name : "Class Folder",
+   type : "Folder",
+   obj : 
+      [{
+         link : "../HTML/classexample.html",
+         name : "My Class"
+      }, {
+         link : "../HTML/classexample.html",
+         name : "My Class"
+      }]
 }]
 
 //window Listeners
@@ -50,20 +50,20 @@ var elements = document.getElementsByClassName("ribbon");
 //Listeners
 //-------------------------Edit Box Listeners------------------------------
 for(var i = 0; i < elements.length; i++) {
- elements[i].addEventListener('click', displayEditBox, false);    
- // elements[i].addEventListener("click", () => clickedId = elements[i].id); 
+  elements[i].addEventListener('click', displayEditBox, false);    
+  // elements[i].addEventListener("click", () => clickedId = elements[i].id); 
 }
 if(elements.length > 0){
- elements[0].addEventListener("click", () => clickedId = elements[0].id); 
- if(elements.length > 1){
-   elements[1].addEventListener("click", () => clickedId = elements[1].id); 
-   if(elements.length > 2){
-     elements[2].addEventListener("click", () => clickedId = elements[2].id); 
-   }
- }
- doneButton.addEventListener("click", () => doneButtonFunction(clickedId));
- removeButton.addEventListener("click", () => removeButtonFunction(clickedId));
- 
+  elements[0].addEventListener("click", () => clickedId = elements[0].id); 
+  if(elements.length > 1){
+    elements[1].addEventListener("click", () => clickedId = elements[1].id); 
+    if(elements.length > 2){
+      elements[2].addEventListener("click", () => clickedId = elements[2].id); 
+    }
+  }
+  doneButton.addEventListener("click", () => doneButtonFunction(clickedId));
+  removeButton.addEventListener("click", () => removeButtonFunction(clickedId));
+  
 }
 
 //-------------------------Edit Box Listeners------------------------------
@@ -76,24 +76,24 @@ if(elements.length > 0){
 //main Activitiy all onload activities
 async function mainActivity() {
 
- await addLinks(sampleListObj);
- setUpBookmarkBar();
- setUpBBBBEditBox();
+  await addLinks(sampleListObj);
+  setUpBookmarkBar();
+  setUpBBBBEditBox();
 
 }
 
 function addLinks(bookmarks) {
- var htmlString = "";
- for (let i = 0; i < bookmarks.length; i++) {
-   if (bookmarks[i].type == "Bookmark") {
-     htmlString = htmlString + "<a href=\"" + bookmarks[i].obj.link + "\" class=\"item\" id=\"" + bookmarks[i].obj.name + "\">" + bookmarks[i].obj.name + "</a>\n";
-   }
- }
- console.log(htmlString);
+  var htmlString = "";
+  for (let i = 0; i < bookmarks.length; i++) {
+    if (bookmarks[i].type == "Bookmark") {
+      htmlString = htmlString + "<a href=\"" + bookmarks[i].obj.link + "\" class=\"item\" id=\"" + bookmarks[i].obj.name + "\">" + bookmarks[i].obj.name + "</a>\n";
+    }
+  }
+  console.log(htmlString);
 
- document.getElementById('links').innerHTML = htmlString;
+  document.getElementById('links').innerHTML = htmlString;
 
- return;
+  return;
 }
 
 //Access bookmark object and remove the clicked one
@@ -103,255 +103,149 @@ function addLinks(bookmarks) {
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function barScroll() {
- // Get the offset position of the navbar
- 
- if (window.pageYOffset >= sticky) {
-   navbar.classList.add("sticky");
- } else {
-   navbar.classList.remove("sticky");
- }
+  // Get the offset position of the navbar
+  
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
 
 
 $(links).bind("contextmenu",function(e){
- return false;
+  return false;
 });
 
 $(document).ready(function(){
- $('#links').mousedown(function(event) {
-   switch(event.which) {
-       case 1:
-           break;
-       case 2:
-           break;
-       case 3:
-         displayBBBBEditBox();
-           break;
-       default:
-           break;
-     }
-   });
+  $('#links').mousedown(function(event) {
+    switch(event.which) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+          displayBBBBEditBox();
+            break;
+        default:
+            break;
+      }
+    });
 });
 
 
 
 function myFunction() {
-   document.getElementById("myDropdown").classList.toggle("show");
- }
- 
- // Close the dropdown if the user clicks outside of it
- window.onclick = function(event) {
-   if (!event.target.matches('.dropbtn')) {
-     var dropdowns = document.getElementsByClassName("dropdown-content");
-     var i;
-     for (i = 0; i < dropdowns.length; i++) {
-       var openDropdown = dropdowns[i];
-       if (openDropdown.classList.contains('show')) {
-         openDropdown.classList.remove('show');
-       }
-     }
-   }
- }
-
-
-
-
-
- newFolderButton.addEventListener("click", () => addBookMarkFolder());
-
- function addBookMarkFolder() {
-   var x = document.getElementById("BookMarkFolderView");
-   var option = document.createElement("option");
-   var name = prompt("Enter Name");
-   option.text = name;
-   x.add(option);
-
- }
-
- function doneButtonFunction(clickedId) {
-   var name = document.getElementById("fname").value;
-   var link = "../HTML/";
-   test = document.getElementById(clickedId);
-   test.src = "../image/FilledBookmark.png";
-
-   if(clickedId.includes('Assignment'))
-   {
-    if(clickedId.includes('SWE'))
-    {
-      link = link + 'assignmentexample.html';
-    }
-    else if(clickedId.includes('Alg'))
-    {
-      link = link + 'alg_assignment.html';
-    }
-    else if(clickedId.includes('OS'))
-    {
-      link = link + 'os_assignment.html';
-    }
-    else if(clickedId.includes('Data'))
-    {
-      link = link + 'databasehw.html';
-    }
-   }
-   else
-   {
-    if(clickedId.includes('SWE'))
-    {
-      link = link + 'classexample.html';
-    }
-    else if(clickedId.includes('Alg'))
-    {
-      link = link + 'algorithms.html';
-    }
-    else if(clickedId.includes('OS'))
-    {
-      link = link + 'os.html';
-    }
-    else if(clickedId.includes('Data'))
-    {
-      link = link + 'DataClass.html';
-    }
-   }
-
-   console.log(clickedId);
-
-   $('#links').append('<a href=\"' + link +'\" class=\"item\" id=\"' + test.id + 'id\">' + name + '</a>\n');
-
-  div = document.getElementById('editBoxContainer');
-  div.style.display = "none";
-}
-
-
-function removeButtonFunction(clickedId) {
-  var rmid = clickedId + 'id';
-  document.getElementById(clickedId).src = "../image/bookmark.png";
-  div = document.getElementById('editBoxContainer');
-  $('#' + rmid).remove();
-  div.style.display = "none";
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
   
-}
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 
 
- function displayEditBox() {
 
 
+
+  newFolderButton.addEventListener("click", () => addBookMarkFolder());
+
+  function addBookMarkFolder() {
+    var x = document.getElementById("BookMarkFolderView");
+    var option = document.createElement("option");
+    var name = prompt("Enter Name");
+    option.text = name;
+    x.add(option);
+
+  }
+
+  function doneButtonFunction(clickedId) {
+    var name = document.getElementById("fname").value;
+    var link = "../HTML/";
+    test = document.getElementById(clickedId);
+    test.src = "../image/FilledBookmark.png";
+
+    if(clickedId.includes('Assignment'))
+    {
+     if(clickedId.includes('SWE'))
+     {
+       link = link + 'assignmentexample.html';
+     }
+     else if(clickedId.includes('Alg'))
+     {
+       link = link + 'alg_assignment.html';
+     }
+     else if(clickedId.includes('OS'))
+     {
+       link = link + 'os_assignment.html';
+     }
+     else if(clickedId.includes('Data'))
+     {
+       link = link + 'databasehw.html';
+     }
+    }
+    else
+    {
+     if(clickedId.includes('SWE'))
+     {
+       link = link + 'classexample.html';
+     }
+     else if(clickedId.includes('Alg'))
+     {
+       link = link + 'algorithms.html';
+     }
+     else if(clickedId.includes('OS'))
+     {
+       link = link + 'os.html';
+     }
+     else if(clickedId.includes('Data'))
+     {
+       link = link + 'DataClass.html';
+     }
+    }
+
+    console.log(clickedId);
+
+    $('#links').append('<a href=\"' + link +'\" class=\"item\" id=\"' + test.id + 'id\">' + name + '</a>\n');
 
    div = document.getElementById('editBoxContainer');
-   if (div.style.display == 'block') {
-     div.style.display = "none";
-   }
-   else {
-     div.style.display = "block";
-
-   }
-
+   div.style.display = "none";
  }
 
+
+ function removeButtonFunction(clickedId) {
+   var rmid = clickedId + 'id';
+   document.getElementById(clickedId).src = "../image/bookmark.png";
+   div = document.getElementById('editBoxContainer');
+   $('#' + rmid).remove();
+   div.style.display = "none";
    
- function displayBBBBEditBox() {
-
-   div = document.getElementById('BBBBeditBoxContainer');
-   if (div.style.display == 'block') {
-     div.style.display = "none";
-   }
-   else {
-     div.style.display = "block";
-
-   }
-
  }
 
 
-
- //------------------------- Bookmark Bar Edit Box Listeners------------------------------
-
-
- let BookMarkId;
-
- function setUpBookmarkBar() {
-   
+  function displayEditBox() {
 
 
-   var BookmarkBarElements = document.getElementById("links"); //Im trying to get all the ids of current bookmarks
 
-   var elements = BookmarkBarElements.getElementsByTagName('a');
+    div = document.getElementById('editBoxContainer');
+    if (div.style.display == 'block') {
+      div.style.display = "none";
+    }
+    else {
+      div.style.display = "block";
 
-   
+    }
 
-  
-
-   for(let j = 0; j < elements.length; j++) { 
-     BookmarkId =  elements[j].id;
-     elements[j].addEventListener("contextmenu", anson = saveBookMarkId.bind(elements[j], elements[j].id)); // Theres was an error here because the event listener is trying to find elements[j].id, which doesn't exist. I don't know how to remove event listener
-     console.log(elements[j].id);                                                                        // I bs'ed it, anson is a reference to the Orignal function, so I am able to remove using removeEventListner. As Javascript does not like functions with parameters. (considered anomoyous function that cant be traced back)
-
-
-   }    
-
- }
-
- function saveBookMarkId (input) {
-
-   BookMarkId = input;
-
- }
-
- function setUpBBBBEditBox() {
-   
-   BBBBdoneButton.addEventListener("click", () => BBBBdoneButtonFunction(BookMarkId));
-   BBBBremoveButton.addEventListener("click", () => BBBBremoveButtonFunction(BookMarkId));
-
- }
-
-
- function removeBookMarkEventListener() {
-   var BookmarkBarElements = document.getElementById("links"); //Im trying to get all the ids of current bookmarks
-
-   var elements = BookmarkBarElements.getElementsByTagName('a');
-
-   
-
-  
-
-   for(let j = 0; j < elements.length; j++) {  
-     elements[j].removeEventListener("contextmenu",  anson);
-     console.log(elements[j].id);
-   }
-
- }
-
- 
-//-------------------------Bookmark Bar Edit Box Listeners------------------------------
-
-
- 
-function BBBBremoveButtonFunction(BookMarkId) {
- var test = document.getElementById(BookMarkId);
-
- console.log(test);
-
-
- removeBookMarkEventListener();
-
- test.remove();
-
- setUpBookmarkBar();
-
-
- div = document.getElementById('BBBBeditBoxContainer');
- div.style.display = "none";
-}
-
-
-function BBBBdoneButtonFunction(BookMarkId) {
- var bookmark = document.getElementById(BookMarkId);
- var nameBox = document.getElementById("fname");
-
- console.log(bookmark.innerHTML);
-
- bookmark.innerHTML = nameBox.value;
-
- 
+  }
 
     
   function displayBBBBEditBox() {
@@ -469,7 +363,4 @@ function BBBBdoneButtonFunction(BookMarkId) {
 
 
 
- div = document.getElementById('BBBBeditBoxContainer');
- div.style.display = "none";
 
-}
