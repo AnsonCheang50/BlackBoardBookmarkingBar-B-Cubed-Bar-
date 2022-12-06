97f8ebaeba7bee7cd145c5ddeaad68c05807eeea
saltyseas69 <116859227+saltyseas69@users.noreply.github.com>
2022-12-05 19:35:47 -0500

    test

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ app.use(
   })
 );
  
-// app.get("/data", (req, res) => {
-//   bookmark.json({msg: 'This is CORS-enabled for all origins!'})
-// });
+app.get("/data", (req, res) => {
+  bookmark.json({msg: 'This is CORS-enabled for all origins!'})
+});
  
-// app.listen("https://bookmarkdb.cloudant.com/bookmarks/c5926a8bdcc095a139c176dbea00b09d");
+app.listen("https://bookmarkdb.cloudant.com/bookmarks/c5926a8bdcc095a139c176dbea00b09d");
